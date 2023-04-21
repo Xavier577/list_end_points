8ed86249d0cfcf78deb1adcabe909e98e80d48c9
Emmanuel Ifeanyi MECHIE <64092981+emmanuelByte@users.noreply.github.com>
2023-04-21 17:10:44 +0100

    fix depency issue

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const listEndpoints = require("list_end_points");
-const app = express();
-const port = 3000;
-
-app.get("/", (req, res) => res.send("Hello World!"));
-listEndpoints.default(app);
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
